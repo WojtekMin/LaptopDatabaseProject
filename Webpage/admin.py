9bc99d08a467cd4da562896551197d9dff9d6e69
Wojciech Mincikiewicz <mincikiewicz@gmail.com>
2018-04-16 10:53:08 +0100

    Addition of more detail way of viewing information in admin site and filter results

--- Webpage/admin.py
+++ Webpage/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
 from .models import Laptop, Processor, GraphicsCard, StorageDrive, RAM, Display
-# Register your models here.
-admin.site.register(Laptop)
-admin.site.register(Processor)
-admin.site.register(GraphicsCard)
-admin.site.register(StorageDrive)
-admin.site.register(RAM)
-admin.site.register(Display)
 
+@admin.register(Laptop)
+class LaptopAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'brand_name', 'processor', 'display_graphics_card', 'ram')
+    list_filter = ('brand_name', 'processor', 'graphics_card')
+
+@admin.register(Processor)
+class ProcessorAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'manufacturer', 'base_clock', 'turbo_clock', 'cores', 'threads')
+    list_filter = ('manufacturer', 'cores', 'threads')
+
+@admin.register(GraphicsCard)
+class GraphicsCardAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'manufacturer', 'type', 'core_base_speed', 'core_turbo_speed', 'memory', 'memory_speed')
+    list_filter = ('manufacturer', 'type', 'memory')
+
+@admin.register(StorageDrive)
+class StorageDriveAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'manufacturer', 'type', 'size', 'read_speed', 'write_speed')
+    list_filter = ('manufacturer', 'type', 'size')
+
+@admin.register(RAM)
+class RAMAdmin(admin.ModelAdmin):
+    list_display = ('type', 'frequency', 'size')
+    list_filter = ('type', 'size', 'frequency')
+
+@admin.register(Display)
+class DisplayAdmin(admin.ModelAdmin):
+    list_display = ('size', 'resolution', 'type')
+    list_filter = ('type', 'size', 'resolution')
--- Webpage/models.py
+++ Webpage/models.py
@@ -2,6 +2,82 @@ from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 
 
+class GraphicsCard(models.Model):
+    model_name = models.CharField(max_length=100)
+
+    MANUFACTURER_TYPE = (
+        ('AMD', 'AMD'),
+        ('Intel', 'Intel'),
+        ('Nvidia', 'Nvidia'),
+    )
+
+    manufacturer = models.CharField(max_length=8, choices=MANUFACTURER_TYPE, default='Intel', blank=False, help_text='Manufacturer type')
+
+    TYPE = (
+        ('d', 'Discrete'),
+        ('i', 'Integrated'),
+    )
+
+    type = models.CharField(max_length=8, choices=TYPE, default='d', blank=False, help_text='Discrete or integrated')
+    architecture = models.CharField(max_length=100)
+    core_base_speed = models.IntegerField(help_text="In MHz")
+    core_turbo_speed = models.IntegerField(help_text="In MHz")
+    memory = models.IntegerField(help_text="In MB")
+    memory_speed = models.IntegerField(help_text="In MHz")
+    memory_bus_width = models.IntegerField(help_text="In Bits")
+    pipelines = models.IntegerField()
+    manufacturing_technology = models.IntegerField(help_text="In nm")
+
+
+    class Meta:
+        ordering = ["model_name"]
+
+
+    def get_absolute_url(self):
+        return reverse('graphics_card-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+        return self.manufacturer + " " + self.model_name
+
+
+class StorageDrive(models.Model):
+    model_name = models.CharField(max_length=100)
+
+    MANUFACTURER_TYPE = (
+        ('Adata', 'Adata'),
+        ('Intel', 'Intel'),
+        ('Kingston', 'Kingston'),
+        ('Samsung', 'Samsung'),
+        ('SanDisk', 'SanDisk'),
+        ('Seagate', 'Seagate'),
+        ('Transcend', 'Transcend'),
+        ('WD', 'WD'),
+    )
+
+    manufacturer = models.CharField(max_length=8, choices=MANUFACTURER_TYPE, default='Samsung', blank=False, help_text='Manufacturer type')
+
+    TYPE = (
+        ('hdd', 'HDD'),
+        ('ssd', 'SSD'),
+    )
+
+    type = models.CharField(max_length=8, choices=TYPE, default='d', blank=False, help_text='HDD or SSD')
+    read_speed = models.IntegerField(help_text="In MB/s")
+    write_speed = models.IntegerField(help_text="In MB/s")
+    size = models.IntegerField(help_text="In GB")
+
+    class Meta:
+        ordering = ["model_name"]
+
+    def get_absolute_url(self):
+        return reverse('storage_drive-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.manufacturer + " " + self.model_name
+
+
+
 class Laptop(models.Model):
     model_name = models.CharField(max_length=200)
 
@@ -33,13 +109,20 @@ class Laptop(models.Model):
     operating_system = models.CharField(max_length=1, choices=OPERATING_SYSTEM, default='w', blank=False, help_text='Opearating system')
     date_of_release = models.DateField()
     processor = models.ForeignKey('Processor', on_delete=models.SET_NULL, null=True)
-    graphics_card = models.ForeignKey('GraphicsCard', on_delete=models.SET_NULL, null=True)
-    storage_drive = models.ForeignKey('StorageDrive', on_delete=models.SET_NULL, null=True)
+    graphics_card = models.ManyToManyField(GraphicsCard)
+    storage_drive = models.ManyToManyField(StorageDrive)
     ram = models.ForeignKey('RAM', on_delete=models.SET_NULL, null=True)
     display = models.ForeignKey('Display', on_delete=models.SET_NULL, null=True)
 
+
+    def display_graphics_card(self):
+        return ', '.join([ GraphicsCard.model_name for GraphicsCard in self.graphics_card.all()[:4] ])
+    display_graphics_card.short_description = 'Graphics card'
+
+
     def __str__(self):
         return self.brand_name + " " + self.model_name
+
 
     def get_absolute_url(self):
         return reverse('laptop-detail', args=[str(self.id)])
@@ -71,77 +154,6 @@ class Processor(models.Model):
     def __str__(self):
         return self.manufacturer + " " + self.model_name
 
-
-class GraphicsCard(models.Model):
-    model_name = models.CharField(max_length=100)
-
-    MANUFACTURER_TYPE = (
-        ('AMD', 'AMD'),
-        ('Intel', 'Intel'),
-        ('Nvidia', 'Nvidia'),
-    )
-
-    manufacturer = models.CharField(max_length=8, choices=MANUFACTURER_TYPE, default='Intel', blank=False, help_text='Manufacturer type')
-
-    TYPE = (
-        ('d', 'Discrete'),
-        ('i', 'Integrated'),
-    )
-
-    type = models.CharField(max_length=8, choices=TYPE, default='d', blank=False, help_text='Discrete or integrated')
-    architecture = models.CharField(max_length=100)
-    core_base_speed = models.IntegerField(help_text="In MHz")
-    core_turbo_speed = models.IntegerField(help_text="In MHz")
-    memory = models.IntegerField(help_text="In MB")
-    memory_speed = models.IntegerField(help_text="In MHz")
-    memory_bus_width = models.IntegerField(help_text="In Bits")
-    pipelines = models.IntegerField()
-    manufacturing_technology = models.IntegerField(help_text="In nm")
-
-    class Meta:
-        ordering = ["model_name"]
-
-    def get_absolute_url(self):
-        return reverse('graphics_card-detail', args=[str(self.id)])
-
-    def __str__(self):
-        return self.manufacturer + " " + self.model_name
-
-
-class StorageDrive(models.Model):
-    model_name = models.CharField(max_length=100)
-
-    MANUFACTURER_TYPE = (
-        ('Adata', 'Adata'),
-        ('Intel', 'Intel'),
-        ('Kingston', 'Kingston'),
-        ('Samsung', 'Samsung'),
-        ('SanDisk', 'SanDisk'),
-        ('Seagate', 'Seagate'),
-        ('Transcend', 'Transcend'),
-        ('WD', 'WD'),
-    )
-
-    manufacturer = models.CharField(max_length=8, choices=MANUFACTURER_TYPE, default='Samsung', blank=False, help_text='Manufacturer type')
-
-    TYPE = (
-        ('hdd', 'HDD'),
-        ('ssd', 'SSD'),
-    )
-
-    type = models.CharField(max_length=8, choices=TYPE, default='d', blank=False, help_text='HDD or SSD')
-    read_speed = models.IntegerField(help_text="In MB/s")
-    write_speed = models.IntegerField(help_text="In MB/s")
-    size = models.IntegerField(help_text="In MB")
-
-    class Meta:
-        ordering = ["model_name"]
-
-    def get_absolute_url(self):
-        return reverse('storage_drive-detail', args=[str(self.id)])
-
-    def __str__(self):
-        return self.manufacturer + " " + self.model_name
 
 
 class RAM(models.Model):
