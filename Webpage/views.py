459a3adb64b770cf31a0191a4881b36ca6726f40
Wojciech Mincikiewicz <mincikiewicz@gmail.com>
2018-04-22 19:22:03 +0100

    Added basic html files for the structure of website

--- Webpage/views.py
+++ Webpage/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Laptop, Processor, GraphicsCard, StorageDrive, RAM, Display
+
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_laptops = Laptop.objects.all().count()
+    num_processors = Processor.objects.all().count()
+    # Available books (status = 'a')
+    num_nvidia_graphics_cards = GraphicsCard.objects.filter(manufacturer='Nvidia').count()
+    num_graphics_cards = GraphicsCard.objects.count()  # The 'all()' is implied by default.
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_laptops': num_laptops, 'num_processors': num_processors,
+                 'num_nvidia_graphics_cards': num_nvidia_graphics_cards, 'num_graphics_cards': num_graphics_cards},
+    )
